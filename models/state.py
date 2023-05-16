c21da50ddadec4f750375fa14915007157a6de67
mumbuamutuku <mumbuamutuku@gmail.com>
2023-05-16 05:01:25 -0700

    state

--- models/state.py
+++ models/state.py
@@ -1,12 +1,13 @@
 #!/usr/bin/python3
 """ State Module for HBNB project """
+import models
 from models.base_model import BaseModel, Base
+import sqlalchemy
 from sqlalchemy import Column, Integer, String, ForeignKey
 from sqlalchemy.orm import relationship
-import models
 from models.city import City
 import os
-
+from os import getenv
 
 class State(BaseModel, Base):
     """ State class """
